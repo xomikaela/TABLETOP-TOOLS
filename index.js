54a309f05062293d012af580e255275918e10b40
mikaelarosee <mikaelarbaker@gmail.com>
2026-07-20 15:45:49 -0700

    completed all commits

---
+++ index.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number} num1 - first added
+ * @param {number} num2 - second added
+ * @returns {number} - the sum of the two addends
+ */
+// function addTwoNums(num1: Number, num2) {
+//    const sum = num1 + num2;
+//    return sum;
+//}
+
+// addTwoNums(3, 5);
+
+let code1;
+code1 = 7 + 3;
+
+let code2 = 160 / 2 - 40;
+
+let code3 = (29 % 10) + 30;
+
+let message = "the vault has been secured. the combination is: ";
+
+let codeA = code1 + "-" + code2 + "-" + code3;
+
+let codeB = `${code1}-${code2}-${code3}`;
+
+console.log(message + codeA + " and " + codeB);
